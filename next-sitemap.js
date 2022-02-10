d605b985b910ad59edcf16d8624deffb4f258871
Aid Thompson <thepandariot@gmail.com>
2022-02-10 06:54:02 +0000

    disallow signup page weird

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,15 @@
-module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://funk-27.co.uk",
-  generateRobotsTxt: true, // (optional)
-  // ...other options
-};
+// module.exports = {
+//   siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://funk-27.co.uk",
+//   generateRobotsTxt: true,
+//   changefreq: "daily",
+//   priority: 0.7,
+//   exclude: ["/signup", "/404", "/affiliates"],
+//   robotsTxtOptions: {
+//     policies: [
+//       {
+//         userAgent: "*",
+//         allow: "/",
+//       },
+//     ],
+//   },
+// };
