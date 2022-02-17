3a82e8306b9cdde0bbee7c5fc80dae4482cf4819
Aid Thompson <thepandariot@gmail.com>
2022-02-17 15:46:35 +0000

    added gtm script

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W925CBM');`,
+          }}
+        ></script>
         <meta
           name="description"
           content="Discontent Providers. Blogs & Podcasts that keep you laughing like a drain while doom-scrolling deeper into the existential abyss"
@@ -16,6 +25,14 @@ export default function Document() {
         <meta name="application-name" content="Funk-27" />
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W925CBM"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
